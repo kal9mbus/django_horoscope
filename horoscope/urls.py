39932269a60c547d2ae8a659939cecda6cba70eb
kal9mbus <50074409+kal9mbus@users.noreply.github.com>
2023-03-25 15:37:34 +1100

    first commit

---
+++ horoscope/urls.py
@@ -0,0 +1,9 @@
+# horoscope urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<int:sign_zodiac>/', views.get_info_about_zodiac_sign_by_number),#
+    path('<str:sign_zodiac>/', views.get_info_about_zodiac_sign),
+
+]
